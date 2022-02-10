52a3c8d707e9db7fa623363bb66fd31abfa1c4a6
Chlodion Gossart <chlodiongossart@gmail.com>
2022-02-10 21:15:38 +0100

    added contact features

--- frontend/view/contact/contact.js
+++ frontend/view/contact/contact.js
@@ -1,3 +1,16 @@
 /**
  * Gère l'affichage et les interactions de la page de contact
  */
+
+document.querySelector('.form input[type="button"]').addEventListener("click",function(){
+  var valid = true;
+  for(let input of document.querySelectorAll(".form imput,.form textarea")){
+    valid &= input.reportValidity();
+    if(!valid){
+      break;
+    }
+  }
+  if(valid){
+    alert("Votre message a bien été envoyé!")
+  }
+});
